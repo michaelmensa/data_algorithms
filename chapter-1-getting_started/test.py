c72f03896e42696ba3daa025e3b0d28844276aaf
michaelmensa <michaelmensa@live.com>
2024-03-11 13:58:02 +0000

    tests

--- chapter-1-getting_started/test.py
+++ chapter-1-getting_started/test.py
@@ -5,4 +5,7 @@ fizzbuzz = __import__('fizzbuzz').fizzbuzz
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
+num = []
+
 print(fizzbuzz(numbers))
+print(fizzbuzz(num))
